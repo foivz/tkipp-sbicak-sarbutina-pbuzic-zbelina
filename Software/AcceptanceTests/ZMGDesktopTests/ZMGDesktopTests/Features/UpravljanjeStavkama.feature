65eefc6c1c2963d0ce26bf1a049f01e4090660d0
sbicak20 <sbicak20@student.foi.hr>
2023-04-10 18:46:18 +0200

    Dodano je automatizirano testiranje dodavanja i brisanja stavki kod izdavanja racuna.

---
+++ Software/AcceptanceTests/ZMGDesktopTests/ZMGDesktopTests/Features/UpravljanjeStavkama.feature
@@ -0,0 +1,42 @@
+﻿Feature: UpravljanjeStavkama
+Feature se odnosi na upravljanje stavkama, gdje radnik moze dodavati, pregledati i obrisati stavke.
+
+#(S13)
+#Kao radnik,
+#kod izdavanja racuna,
+#zelim dodati stavke na racun.
+
+Background:
+	Given Korisniku se otvara forma za upravljanje stavkama
+
+Scenario: Dodati stavku na racun bez potrebnog unosa
+When Korisnik klikne na gumb Dodaj
+Then Korisniku se izbacuje poruka sustava za unos stavke
+And Korisnik klikne na gumb Ok
+
+
+Scenario: Dodati stavku na racun s krivim unosom
+When Korisnik unese krive podatke
+Then Korisnik klikne na gumb Dodaj
+And Korisniku se izbacuje poruka sustava za unos stavke
+And Korisnik klikne na gumb Ok
+
+
+Scenario: Dodati stavku s tocnim odredenim unosom
+When Korisnik unese tocne podatke
+Then Korisnik klikne na gumb Dodaj
+
+
+Scenario: Dodati stavku s tocnim odredenim unosom ponovno
+When Korinsik ponovno unese tocne podatke
+Then Korisnik klikne na gumb Dodaj
+And Korisniku se izbacuje poruka sustava za unos stavke
+And Korisnik klikne na gumb Ok 
+
+
+Scenario: Obrisati odredenu stavku iz polja
+When Korisnik oznaci stavku
+Then Korisnik klikne na gumb Obrisi
+
+
+	
---
+++ Software/AcceptanceTests/ZMGDesktopTests/ZMGDesktopTests/Support/GuiDriverAppOpen.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ZMGDesktopTests.Support
+{
+    public static class GuiDriverAppOpen
+    {
+        private static WindowsDriver<WindowsElement> driver;
+        public static WindowsDriver<WindowsElement> GetOrCreateDriver()
+        {
+            if (driver == null)
+            {
+                driver = CreateDriverInstance();
+            }
+            return driver;
+        }
+        public static WindowsDriver<WindowsElement> GetDriver()
+        {
+            return driver;
+        }
+        private static WindowsDriver<WindowsElement> CreateDriverInstance()
+        {
+            IntPtr myAppTopLevelWindowsHandle = new IntPtr();
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains("ZMGDesktop"))
+                {
+                    myAppTopLevelWindowsHandle = clsProcess.MainWindowHandle;
+                    break;
+                }
+            }
+            var appTopLevelWindowsHandleHex = myAppTopLevelWindowsHandle.ToString("x");
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("platformName", "Windows");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            if (appTopLevelWindowsHandleHex == "0")
+            {
+                //Ukoliko je heksadecimalna vrijdenost nula, znaci da nije pokrenut proces sa zadanim imenom te se pokrece sa diska
+                options.AddAdditionalCapability("app", @"C:\Winatjecaj\Winatjecaj.exe");
+            }
+            else
+            {
+                //Aplikacije je pokrenuta
+                options.AddAdditionalCapability("appTopLevelWindow", appTopLevelWindowsHandleHex);
+            }
+            var wd = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            wd.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return wd;
+        }
+        public static void Dispose()
+        {
+            if (driver != null)
+            {
+                foreach (var wh in driver.WindowHandles)
+                {
+                    driver.SwitchTo().Window(wh);
+                    driver.CloseApp();
+                }
+                driver.Quit();
+                driver.Dispose();
+                driver = null;
+            }
+        }
+
+    }
+}
