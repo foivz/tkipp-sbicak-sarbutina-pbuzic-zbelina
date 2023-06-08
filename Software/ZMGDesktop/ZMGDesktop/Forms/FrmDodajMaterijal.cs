92ec9108df62f03a8f16e8126fbf475147da3917
sarbutina20 <sarbutina20@foi.hr>
2023-06-08 21:31:03 +0200

    Unapređenje koda forme dodavanja materijala: odvajanje odgovornosti glavne metode na više metoda i svaka metoda provjerava određeni aspekt unosa. Glavna metoda provjeriUnos poziva ove metode i prekida izvođenje ako se dogodi neuspjeh u provjeri. Ostale promjene uključuju razdvajanje odgovornosti metode btnDodajMaterijal_Click na više metoda koja sada samo poziva druge metode koje izvršavaju određene zadatke, uklonjena je bespotrebna varijabla validacija jer se koristi samo u jednoj metodi i jako puno stilskih promjena koje znatno povećavaju preglednost koda.

--- Software/ZMGDesktop/ZMGDesktop/Forms/FrmDodajMaterijal.cs
+++ Software/ZMGDesktop/ZMGDesktop/Forms/FrmDodajMaterijal.cs
@@ -1,110 +1,116 @@
 ﻿using BusinessLogicLayer.Services;
-using DataAccessLayer.Iznimke;
 using ZMGDesktop.ValidacijaUnosa;
 using EntitiesLayer.Entities;
-using QRCoder;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
-using ZMGDesktop.ValidacijaUnosa;
 using static EntitiesLayer.Entities.Enumeracije;
-using System.Windows.Markup;
-using System.Drawing;
 
-namespace ZMGDesktop
-{
-    public partial class FrmDodajMaterijal : Form
-    {
-        MaterijalServices matServis = new MaterijalServices();
+
+namespace ZMGDesktop {
+    public partial class FrmDodajMaterijal : Form {
+        private MaterijalServices matServis = new MaterijalServices();
         private Validacija validacija = new Validacija();
-        public FrmDodajMaterijal()
-        {
+
+        public FrmDodajMaterijal() {
             InitializeComponent();
             ucitajPomoc();
         }
 
-        private void ucitajPomoc()
-        {
+        private void ucitajPomoc() {
             this.KeyPreview = true;
             this.KeyDown += new KeyEventHandler(Form1_KeyDown);
         }
 
-        private void btnNatrag_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void FrmDodajMaterijal_Load(object sender, EventArgs e)
-        {
+        private void FrmDodajMaterijal_Load(object sender, EventArgs e) {
             UcitajMjerneJedinice();
         }
 
-        private void UcitajMjerneJedinice()
-        {
+        private void UcitajMjerneJedinice() {
             cmbMjernaJedinica.DataSource = Enum.GetValues(typeof(MjerneJedinice));
         }
 
-        private void btnDodajMaterijal_Click(object sender, EventArgs e)
-        {
-            if(provjeriUnos()) {
-                string naziv = txtNaziv.Text;
-                int kolicina = (int)txtKolicina.Value;
-                string odabranaJedinica = cmbMjernaJedinica.SelectedItem.ToString();
-                float cijena = (float)txtCijena.Value;
-                string opis = txtOpis.Text;
-                bool opasno = txtOpasno.Checked;
+        private void btnNatrag_Click(object sender, EventArgs e) {
+            this.Close();
+        }
 
-                string qr_kod = GenerirajRandomString();
-
-
-
-                Materijal noviMaterijal = new Materijal
-                {
-                    Naziv = naziv,
-                    Kolicina = kolicina,
-                    JedinicaMjere = odabranaJedinica,
-                    CijenaMaterijala = cijena,
-                    Opis = opis,
-                    OpasnoPoZivot = opasno,
-                    QR_kod = qr_kod
-                };
-                matServis.dodajMaterijal(noviMaterijal);
-                this.Close();
+        private void btnDodajMaterijal_Click(object sender, EventArgs e) {
+            if (provjeriUnos()) {
+                Materijal noviMaterijal = KreirajNoviMaterijal();
+                DodajMaterijal(noviMaterijal);
+                ZatvoriFormu();
             }
         }
 
+        private Materijal KreirajNoviMaterijal() {
+            string naziv = txtNaziv.Text;
+            int kolicina = (int)txtKolicina.Value;
+            string odabranaJedinica = cmbMjernaJedinica.SelectedItem.ToString();
+            float cijena = (float)txtCijena.Value;
+            string opis = txtOpis.Text;
+            bool opasno = txtOpasno.Checked;
+            string qr_kod = GenerirajRandomString();
 
-        string GenerirajRandomString()
-        {
+            Materijal noviMaterijal = new Materijal {
+                Naziv = naziv,
+                Kolicina = kolicina,
+                JedinicaMjere = odabranaJedinica,
+                CijenaMaterijala = cijena,
+                Opis = opis,
+                OpasnoPoZivot = opasno,
+                QR_kod = qr_kod
+            };
+
+            return noviMaterijal;
+        }
+
+        private void DodajMaterijal(Materijal materijal) {
+            matServis.dodajMaterijal(materijal);
+        }
+
+        private void ZatvoriFormu() {
+            this.Close();
+        }
+
+        private string GenerirajRandomString() {
             Random random = new Random();
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, 20)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
+        private bool provjeriUnos() {
+            if (!ProvjeriPolja())
+                return false;
 
-        private bool provjeriUnos()
-        {
-            if (txtNaziv.Text == "" || txtCijena.Value == 0 || txtKolicina.Value == 0 || cmbMjernaJedinica.SelectedItem == null || txtOpis.Text == null)
-            {
+            if (!ProvjeriNaziv())
+                return false;
+
+            if (!ProvjeriCijenu())
+                return false;
+
+            return true;
+        }
+
+        private bool ProvjeriPolja() {
+            if (txtNaziv.Text == "" || txtCijena.Value == 0 || txtKolicina.Value == 0 || cmbMjernaJedinica.SelectedItem == null || string.IsNullOrEmpty(txtOpis.Text)) {
                 MessageBox.Show("Potrebno je ispuniti sva polja", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
-            if (!validacija.provjeraSamoSlova(txtNaziv.Text))
-            {
+            return true;
+        }
+
+        private bool ProvjeriNaziv() {
+            if (!validacija.provjeraSamoSlova(txtNaziv.Text)) {
                 MessageBox.Show("Naziv može sadržavati samo slova");
                 return false;
             }
+            return true;
+        }
+
+        private bool ProvjeriCijenu() {
             if (!validacija.provjeraSamoBrojevi(txtCijena.Value.ToString())) {
                 MessageBox.Show("Cijena može sadržavati samo brojeve");
                 return false;
@@ -112,13 +118,12 @@ namespace ZMGDesktop
             return true;
         }
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.F1)
-            {
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.F1) {
                 string path = Path.Combine(Application.StartupPath, "Pomoc\\Pomoc\\Skladiste\\DodajMaterijal\\dodajMaterijal.html");
                 System.Diagnostics.Process.Start(path);
             }
         }
     }
 }
+
