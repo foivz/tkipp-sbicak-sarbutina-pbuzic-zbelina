933b9c19558fd4f9b96e03be84b5fd8bb93f5c23
sbicak20 <sbicak20@student.foi.hr>
2023-06-09 19:13:30 +0200

    PoslodavacRepository je 100% pokriven s FCCom.

--- Software/ZMGDesktop/ZMG.IntegrationTests/sbicak20_Integration/PoslodavacRepository_Integration_Tests.cs
+++ Software/ZMGDesktop/ZMG.IntegrationTests/sbicak20_Integration/PoslodavacRepository_Integration_Tests.cs
@@ -1,32 +1,49 @@
 ﻿using BusinessLogicLayer.Services;
 using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ZMG.IntegrationTests.sbicak20_Integration
 {
     public class PoslodavacRepository_Integration_Tests
     {
-            private KlijentServices _klijentServices;
-            private RadnikServices RadnikServices;
-            private RobaService RobaService;
-            private UslugaServices UslugaServices;
-            private PoslodavacServices PoslodavacServices;
-            private RacunService RacunService;
-            private StavkaRacunService StavkaRacunService;
+        private PoslodavacServices PoslodavacServices;
 
-            private void kreirajServis()
-            {
-                _klijentServices = new KlijentServices(new KlijentRepository());
-                RadnikServices = new RadnikServices(new RadnikRepository());
-                RobaService = new RobaService(new RobaRepository());
-                UslugaServices = new UslugaServices(new UslugaRepository());
-                PoslodavacServices = new PoslodavacServices(new PoslodavacRepository());
-                RacunService = new RacunService(new RacunRepository());
-                StavkaRacunService = new StavkaRacunService(new StavkaRepository());
-            }
+        private void kreirajServis()
+        {
+            PoslodavacServices = new PoslodavacServices(new PoslodavacRepository());
+        }
+
+        [Fact]
+        public void DohvatiPoslodavca_PoslodavacPostojiUBazi_PoslodavacJeDohvacen()
+        {
+            //arrange
+            kreirajServis();
+            //act
+            Poslodavac poslodavac = PoslodavacServices.GetPoslodavac();
+            //assert
+            Assert.NotNull(poslodavac);
+            Assert.Equal(1, poslodavac.Poslodavac_ID);
+            Assert.Equal("Republika Hrvatska", poslodavac.Drzava);
+        }
+
+        [Fact]
+
+        public void Update_NijeImpelementiranaFunkcija_BacaSeGreska()
+        {
+            //arrange
+            var poslodavac = new Poslodavac();  
+            var poslodavacRepo = new PoslodavacRepository();
+            //act
+            Action act = () => poslodavacRepo.Update(poslodavac);
+            //assert
+            Assert.Throws<NotImplementedException>(() => act());
+        }
+
     }
 }
