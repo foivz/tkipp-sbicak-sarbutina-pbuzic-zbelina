4cda121ae642298142be3aa746ef16264757bbd5
sbicak20 <sbicak20@student.foi.hr>
2023-06-09 17:58:05 +0200

    Dodan jedinicni testni slucaj za klasu Poslodavac. Testni slucaj provjerava servis PoslodavacService i metodu GetPoslodavac.

--- Software/ZMGDesktop/ZMGDesktop_Tests/sbicak20/PoslodavacServices_Tests.cs
+++ Software/ZMGDesktop/ZMGDesktop_Tests/sbicak20/PoslodavacServices_Tests.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using FakeItEasy;
+using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using BusinessLogicLayer.Services;
 
 namespace ZMGDesktop_Tests.sbicak20
 {
@@ -13,10 +17,28 @@ namespace ZMGDesktop_Tests.sbicak20
         public void GetPoslodavac()
         {
             //arrange
+            var fakeRepo = A.Fake<IPoslodavacRepository>();
+            var fakePoslodavac = new List<Poslodavac>
+            {
+                new Poslodavac
+                {
+                    Poslodavac_ID = 1,
+                    OIB = "123",
+                    Naziv = "ZMG"
+                }
+                
+            };
 
+            var poslodavacService = new PoslodavacServices(fakeRepo);
+            A.CallTo(() => fakeRepo.DohvatiPoslodavca(1)).Returns(fakePoslodavac.AsQueryable());
             //act
-
+            var poslodavac = poslodavacService.GetPoslodavac();
             //assert
+            Assert.NotNull(poslodavac);
+            Assert.IsType<Poslodavac>(poslodavac);
+            Assert.Equal("123", poslodavac.OIB);
+            Assert.Equal("ZMG", poslodavac.Naziv);
+            Assert.Equal(1, poslodavac.Poslodavac_ID);
         }
     }
 }
