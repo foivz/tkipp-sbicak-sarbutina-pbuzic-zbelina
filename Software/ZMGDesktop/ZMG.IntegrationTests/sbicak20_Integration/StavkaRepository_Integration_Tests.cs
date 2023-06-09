a3708c1e084fe4bbdcb3758468c48d4b85a244b2
sbicak20 <sbicak20@student.foi.hr>
2023-06-09 19:40:41 +0200

    Napravljeni su integracijski testovi koji pokrivaju klasu StavkaRepository s 100% code coveragea.

--- Software/ZMGDesktop/ZMG.IntegrationTests/sbicak20_Integration/StavkaRepository_Integration_Tests.cs
+++ Software/ZMGDesktop/ZMG.IntegrationTests/sbicak20_Integration/StavkaRepository_Integration_Tests.cs
@@ -32,45 +32,59 @@ namespace ZMG.IntegrationTests.sbicak20_Integration
             }
 
         [Fact]
+        public void Update_NijeImpelementiranaFunkcija_BacaSeGreska()
+        {
+            //arrange
+            var stavka = new StavkaRacun();
+            var stavkaRepo = new StavkaRepository();
+            //act
+            Action act = () => stavkaRepo.Update(stavka);
+            //assert
+            Assert.Throws<NotImplementedException>(() => act());
+        }
+
+        [Fact]
         public void InitStavka_ProsljedujeSeStavkaRobaIUsluga_StavkaJeInicijalizirana()
         {
-            
+
             //arrange
-           
+            kreirajServis();
+            var stavka = new StavkaRacun
+            {
+                KolikoRobePoJedinici = 123,
+                KolicinaRobe = 123,
+                DatumIzrade = DateTime.Now,
+                JedinicaMjere = "kg",
+                JedinicnaCijena = 123,
+                UkupnaCijenaStavke = 123
+            };
+            var usluga = UslugaServices.DohvatiUsluguPoNazivu("Cincanje");
+            var roba = new Roba
+            {
+                Roba_ID = 181
+            };
+
             //act
+            var novaStavka = StavkaRacunService.InitStavka(stavka, roba, usluga);
 
             //assert
-        
+            Assert.NotNull(novaStavka);
+            Assert.Equal(123, novaStavka.JedinicnaCijena);
+            Assert.Equal("Cincanje", novaStavka.Usluga.Naziv);
+            Assert.Equal("Šipka e2", novaStavka.Roba.Naziv);
         }
 
         [Fact]
         public void DohvatiStavkeRacuna_DohvacajuSeStavkePremaIDRacunu_SveStavkeSuDohvacene()
         {
             //arrange
-           
+            kreirajServis();
             //act
-
+            var listaStavkiRacuna = StavkaRacunService.DohvatiStavkeRacuna(158);
             //assert
-        }
-
-        [Fact]
-        public void ProvjeriDuplikat_ProsljedenaJeListaIRazlicitaStavka_DuplikatPostoji()
-        {
-            //arrange
-
-            //act
-
-            //assert
-        }
-
-        [Fact]
-        public void ProvjeriDuplikat_ProsljedenaJeListaIRazlicitaStavka_DuplikatNePostoji()
-        {
-            //arrange
-
-            //act
-
-            //assert
+            Assert.NotEmpty(listaStavkiRacuna);
+            Assert.Equal(1, listaStavkiRacuna.Count);
+            Assert.Equal(150, listaStavkiRacuna[0].KolicinaRobe);
         }
     }
 }
